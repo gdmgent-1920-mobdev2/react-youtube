ad2353f4b25cda82d35d832ca2fc78807c8a2105
unknown <Administrator@DMARB0201C687WX.student.arteveldehs.be>
2020-02-28 09:25:51 +0100

    Add .env variables and the corresponding config file

--- client/src/app/context/YoutubeApiContext.js
+++ client/src/app/context/YoutubeApiContext.js
@@ -1,17 +1,18 @@
 import React, { createContext, useEffect, useState } from 'react';
 
+import { youtubeDataApi } from '../config';
+
 const YoutubeApiContext = createContext();
 
 const YoutubeApiProvider= ({children}) => { 
     const [ searchResults, setSearchResults ] = useState([]);
 
-    const YOUTUBE_API_SEARCH = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyDMtqcgbO-Ix7fOJc-plvQ7YmSjB0C14WI&part=snippet&maxResults=50&q=';
+    const YOUTUBE_API_SEARCH = `${youtubeDataApi.searchUrl}?key=${youtubeDataApi.apiKey}&part=snippet&maxResults=20&q=`;
 
     const search = async (q) => {
         const url = `${YOUTUBE_API_SEARCH}${q}`;
         const response = await fetch(url);
         const jsonData = await response.json();
-        console.log(jsonData);
         setSearchResults(jsonData);
     }   
 
