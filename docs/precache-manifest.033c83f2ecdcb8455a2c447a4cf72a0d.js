95a57479d1a9751640408146c9783f8d3a03d06d
unknown <Administrator@DMARB0201C687WX.student.arteveldehs.be>
2020-02-28 11:27:13 +0100

    Add homepage to package.json

---
+++ docs/precache-manifest.033c83f2ecdcb8455a2c447a4cf72a0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "add64e3838fc6c89e57608b8da893020",
+    "url": "/react-youtube/index.html"
+  },
+  {
+    "revision": "8dc7aa926e91c4987ad5",
+    "url": "/react-youtube/static/css/main.f5777ee9.chunk.css"
+  },
+  {
+    "revision": "f2cfc5bb5ff99d5b4bc3",
+    "url": "/react-youtube/static/js/2.9318c583.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-youtube/static/js/2.9318c583.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dc7aa926e91c4987ad5",
+    "url": "/react-youtube/static/js/main.1080fee9.chunk.js"
+  },
+  {
+    "revision": "6a1dc09dbe9e5d903fae",
+    "url": "/react-youtube/static/js/runtime-main.033c7b57.js"
+  }
+]);
